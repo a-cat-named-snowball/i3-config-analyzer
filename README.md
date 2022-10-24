10f4cd312935819720f6b2ec1b55003487cf9573
a-cat-named-snowball <12f32@protonmail.com>
2022-10-24 16:57:47 -0400

    Added command line arguments to specify path and to show only missing executables

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
 # i3-config-analyzer
-Parses i3 config and extracts the list of required executables
+- Parses i3 config and extracts the list of required executables.
+- Capable of printing a list of executables required by your i3 config but not found on your system.
+
+Usage: i3-config-analyzer [OPTIONS]
+
+Options:
+  -m, --missing                Show only executables that are missing from your system
+  -p, --path <I3 CONFIG FILE>  Path of i3 config file, uses default location if omitted
+  -h, --help                   Print help information
+  -V, --version                Print version information
--- src/main.rs
+++ src/main.rs
@@ -1,26 +1,80 @@
+use anyhow::anyhow;
+use clap::Parser;
 use dirs::config_dir;
 use regex::Regex;
 use std::collections::HashSet;
+use which::which;
 
-fn main() {
-    // TODO: Allow user to specify file location
-    // TODO: Display better error message if file isn't found
+#[derive(Parser, Debug)]
+#[command(author, version, about, long_about = None)]
+struct Args {
+    /// Show only executables that are missing from your system
+    #[arg(short, long)]
+    missing: bool,
 
-    let mut path = config_dir().expect("No config dir found");
-    path.push("i3");
-    path.push("config");
-    //dbg!(path);
+    /// Path of i3 config file, uses default location if omitted
+    #[arg(short, long, value_name = "I3 CONFIG FILE")]
+    path: Option<std::path::PathBuf>,
+}
 
-    let cfg = std::fs::read_to_string(path.as_os_str()).expect("i3 config not found.");
+#[derive(Debug, PartialEq, Eq, Hash)]
+struct Executable {
+    name: String,
+    found: bool,
+}
+
+fn print_all(execs: &HashSet<Executable>) {
+    execs.iter().for_each(|exe| {
+        println!("{}", exe.name);
+    })
+}
+
+fn print_missing(execs: &HashSet<Executable>) {
+    execs.iter().filter(|exe| !exe.found).for_each(|exe| {
+        println!("{}", exe.name);
+    })
+}
+
+fn main() -> anyhow::Result<()> {
+    let args = Args::parse();
+
+    let path = match args.path {
+        None => {
+            let mut path = config_dir().map_or_else(
+                || Err(anyhow!("Default i3 path could not be found. XDG directories may be configured improperly. Try passing the path manually with the -p flag.")),
+                |v| Ok(v)
+            )?;
+            path.push("i3");
+            path.push("config");
+            path
+        }
+        Some(p) => {
+            let mut path = std::path::PathBuf::new();
+            path.push(p);
+            path
+        }
+    };
+
+    let cfg = std::fs::read_to_string(path.as_os_str()).map_err(|_| anyhow!("Invalid path"))?;
 
     let re = Regex::new(r#"(exec (-.*? )?"?|\$\()(?P<name>.*?)( |"|$)"#).unwrap();
 
     let mut execs = HashSet::new();
     cfg.split("\n").for_each(|line| {
         for caps in re.captures_iter(line) {
-            execs.insert(caps["name"].to_string());
+            let name = caps["name"].to_string();
+            execs.insert(Executable {
+                found: which(&name).map_or_else(|_| false, |_| true),
+                name,
+            });
         }
     });
 
-    println!("{}", execs.into_iter().collect::<Vec<String>>().join("\n"));
+    if args.missing {
+        print_missing(&execs);
+    } else {
+        print_all(&execs);
+    }
+
+    Ok(())
 }
